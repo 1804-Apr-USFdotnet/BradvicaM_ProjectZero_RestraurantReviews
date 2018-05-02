f14d021369d83a4e109da58f70de2ec8b7999806
Michael B <mjbradvica@users.noreply.github.com>
2018-05-02 10:20:14 -0400

    add restaurant
    add restaurant

--- RestraurantReviews/RR.ViewModels/AddRestaurantViewModel.cs
+++ RestraurantReviews/RR.ViewModels/AddRestaurantViewModel.cs
@@ -1,13 +1,31 @@
-﻿namespace RR.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RR.ViewModels
 {
     public class AddRestaurantViewModel
     {
+        [Required]
         public string Name { get; set; }
+
+        [Required]
         public string Street { get; set; }
+
+        [Required]
         public string City { get; set; }
+
+        [Required]
         public string State { get; set; }
+
+        [Required]
+        [DataType(DataType.PostalCode)]
         public int ZipCode { get; set; }
+
+        [Required]
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Not a valid phone number")]
         public string PhoneNumber { get; set; }
+
+        [Required]
         public string Website { get; set; }
     }
 }
